7ab1cb81a0bc29a5d06f08453d9142c74d7c2e88
kas-113 <allakravtsova.113@gmail.com>
2026-05-14 01:08:15 +0300

    rf jenkins_utils.py, wait-for-jenkins.sh

--- .github/wait-for-jenkins.sh
+++ .github/wait-for-jenkins.sh
@@ -1,18 +1,45 @@
+##!/bin/bash
+#
+#set -e
+#
+#times=25
+#while ! curl -sSL 'http://localhost:8080/login?from=%2F' 2>&1 \
+#             | grep 'Username' >/dev/null; do
+#    echo 'Waiting for the Jenkins'
+#    sleep 10
+#    times=$(($times - 1))
+#
+#    if [ $times -le 0 ]; then
+#        echo 'Time out'
+#        exit 1
+#    fi
+#done
+#
+#echo 'The Jenkins is up'
+
 #!/bin/bash
 
 set -e
 
-times=25
-while ! curl -sSL 'http://localhost:8080/login?from=%2F' 2>&1 \
-             | grep 'Username' >/dev/null; do
-    echo 'Waiting for the Jenkins'
+times=40
+
+while true; do
+    response=$(curl -u "$JENKINS_USERNAME:$JENKINS_PASSWORD" \
+        -s -o /dev/null -w "%{http_code}" \
+        http://localhost:8080/crumbIssuer/api/json)
+
+    if [ "$response" = "200" ]; then
+        echo "Jenkins is fully ready"
+        break
+    fi
+
+    echo "Waiting for Jenkins API... status=$response"
+
     sleep 10
-    times=$(($times - 1))
+    times=$((times - 1))
 
     if [ $times -le 0 ]; then
-        echo 'Time out'
+        echo "Timeout waiting for Jenkins"
         exit 1
     fi
 done
-
-echo 'The Jenkins is up'
