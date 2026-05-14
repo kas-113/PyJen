9185471bff1c59bf5bf8f570f85e475179264627
kas-113 <allakravtsova.113@gmail.com>
2026-05-14 17:16:05 +0300

    rf wait-for-jenkins.sh to the init state

--- .github/wait-for-jenkins.sh
+++ .github/wait-for-jenkins.sh
@@ -16,30 +16,3 @@ while ! curl -sSL 'http://localhost:8080/login?from=%2F' 2>&1 \
 done
 
 echo 'The Jenkins is up'
-
-#!/bin/bash
-
-#set -e
-#
-#times=40
-#
-#while true; do
-#    response=$(curl -u "$JENKINS_USERNAME:$JENKINS_PASSWORD" \
-#        -s -o /dev/null -w "%{http_code}" \
-#        http://localhost:8080/crumbIssuer/api/json)
-#
-#    if [ "$response" = "200" ]; then
-#        echo "Jenkins is fully ready"
-#        break
-#    fi
-#
-#    echo "Waiting for Jenkins API... status=$response"
-#
-#    sleep 10
-#    times=$((times - 1))
-#
-#    if [ $times -le 0 ]; then
-#        echo "Timeout waiting for Jenkins"
-#        exit 1
-#    fi
-#done
