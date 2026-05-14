357153bc1c9e9fe3331bd9c721cbb5ff908c9f94
kas-113 <allakravtsova.113@gmail.com>
2026-05-14 20:10:42 +0300

    rf test_create_freestyle_project

--- tests/test_freestyle_project.py
+++ tests/test_freestyle_project.py
@@ -4,17 +4,16 @@ from selenium.webdriver.common.by import By
 FR_PROJECT_NAME = "My Freestyle"
 
 def test_create_freestyle_project(browser):
-    project_name = "My Freestyle"
     browser.find_element(By.XPATH, "//a[@href='newJob']").click()
 
-    browser.find_element(By.ID, "name").send_keys(project_name)
+    browser.find_element(By.ID, "name").send_keys(FR_PROJECT_NAME)
     browser.find_element(By.XPATH, "//span[contains(text(), 'Freestyle project')]").click()
     browser.find_element(By.ID, "ok-button").click()
 
     browser.find_element(By.XPATH, "//button[@value='Save']").click()
 
     actual_text = browser.find_element(By.CSS_SELECTOR, ".job-index-headline.page-headline").text
-    assert actual_text == project_name
+    assert actual_text == FR_PROJECT_NAME
 
 
 def test_add_description_when_configure(browser):
